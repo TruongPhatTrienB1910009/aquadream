4648c573497a652d3bdabc8a48b73082d6980d0b
TruongPhatTrienB1910009 <phattrientruong15062001@gmail.com>
2023-07-10 17:32:25 +0700

    [tokenId].tsx

--- pages/sell.tsx
+++ pages/sell.tsx
@@ -10,7 +10,7 @@ import NFTGrid from "../components/NFT/NFTGrid";
 import tokenPageStyles from "../styles/Token.module.css";
 import { NFT as NFTType } from "@thirdweb-dev/sdk";
 import SaleInfo from "../components/SaleInfo/SaleInfo";
-import { GetNFTs } from "../components/NFT/hook/getNFTs"
+import { GetNFTs } from "../components/NFT/hook/getNFTs";
 
 
 const Sell = () => {
