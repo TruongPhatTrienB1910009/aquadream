5ee9255f18ed3aec881e8e3916791d16026b4957
TruongPhatTrienB1910009 <phattrientruong15062001@gmail.com>
2023-07-11 17:02:33 +0700

    fix buy

--- components/NFT/NFTGrid.tsx
+++ components/NFT/NFTGrid.tsx
@@ -30,15 +30,15 @@ const NFTGrid = ({
         data.map((nft) =>
           !overrideOnclickBehavior ? (
             <Link
-              href={`/token/${nft.metadata.address}/${nft.id}`}
-              key={nft.id}
+              href={`/token/${nft.metadata.address}/${nft.tokenId}`}
+              key={nft.tokenId}
               className={styles.nftContainer}
             >
               <NFTComponent nft={nft} />
             </Link>
           ) : (
             <div
-              key={nft.id}
+              key={nft.tokenId}
               className={styles.nftContainer}
               onClick={() => overrideOnclickBehavior(nft)}
             >
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -59,7 +59,7 @@ export function Navbart() {
                 </div>
               </Navbar.Brand>
               <div className={styles.navLeft}>
-                
+
               </div>
               <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-sm`} />
               <Navbar.Offcanvas
@@ -73,7 +73,7 @@ export function Navbart() {
                   className="justify-content-end"
                 ></Offcanvas.Header>
                 <Offcanvas.Body>
-                  <Nav className="justify-content-start flex-grow-1 pe-3">
+                  <Nav className="justify-content-start flex-grow-1 pe-3" style={{ alignItems: "center" }}>
                     <Nav.Link
                       style={{
                         color: "black",
@@ -137,103 +137,5 @@ export function Navbart() {
         </nav>
       </div>
     </>
-    // <div className={styles.navContainer}>
-    //   <nav className={styles.nav}>
-    // <div className={styles.navLeft}>
-    //       <div
-    //         style={{
-    //           display: "flex",
-    //           alignItems: "center",
-    //         }}
-    //       >
-    // <Link href="/" style={{ color: "black" }}>
-    //   <FontAwesomeIcon
-    //     icon={faEthereum}
-    //     style={{
-    //       padding: "10px 15px 10px 15px",
-    //       backgroundColor: "#c0ff3e",
-    //       borderRadius: "10px",
-    //       fontSize: "25px",
-    //     }}
-    //   />
-    // </Link>
-    // <Link href="/" style={{ textDecoration: "none" }}>
-    //   <span className={styles.navbarTittleNfts}>AquaDream</span>
-    // </Link>
-    //         <div className={styles.navbarSearch}>
-    //         <FontAwesomeIcon
-    //           icon={faMagnifyingGlass}
-    //           style={{
-    //             padding: "10px 15px 10px 15px",
-    //             backgroundColor: "#e3e5e8",
-    //             borderRadius: "50%",
-    //             WebkitBorderRadius: "50%",
-    //             MozBorderRadius: "50%",
-    //             fontSize: "20px",
-    //             fontWeight: "100",
-    //             marginRight: '1rem'
-    //           }}
-    //         />
-    //         </div>
-
-    //         <div className={styles.navMiddle}>
-    //           <Link href="/buy" className={styles.link}>
-    //             Buy
-    //           </Link>
-    //           <Link href="/sell" className={styles.link}>
-    //             Sell
-    //           </Link>
-    //           <Link href="/minigame" style={{}} className={styles.link}>
-    //             Mini Game
-    //           </Link>
-    //         </div>
-    //       </div>
-    //     </div>
-
-    //     <div className={styles.navRight}>
-    //       <div className={styles.dropDownMenu}>
-    //         <div className={styles.dropdown}>
-    //           <span onClick={() => showMenu()} className={styles.menuText}>
-    //             <FontAwesomeIcon
-    //               style={{ fontSize: "2em", color: "#dad7d7" }}
-    //               icon={faBars}
-    //             />
-    //           </span>
-    //           <div id="dropdown" className={styles.dropdown_content}>
-    //             <Link href="/buy" className={styles.link}>
-    //               Buy
-    //             </Link>
-    //             <Link href="/sell" className={styles.link}>
-    //               Sell
-    //             </Link>
-    //             <Link href="/minigame" className={styles.link}>
-    //               Mini Game
-    //             </Link>
-    //           </div>
-    //         </div>
-    //       </div>
-    //       <div className={styles.navConnect}>
-    //         <ConnectWallet
-    //           style={{ backgroundColor: "black", color: "white" }}
-    //           theme="dark"
-    //           btnTitle="Connect Wallet"
-    //         />
-    //       </div>
-    //       {address ? (
-    //         <Link className={styles.link} href={`/profile/${address}`}>
-    //           <Image
-    //             className={styles.profileImage}
-    //             src="/user-icon.png"
-    //             width={42}
-    //             height={42}
-    //             alt="Profile"
-    //           />
-    //         </Link>
-    //       ) : (
-    //         ""
-    //       )}
-    //     </div>
-    //   </nav>
-    // </div>
   );
 }
