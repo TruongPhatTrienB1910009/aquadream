f3ccf7f6cc7668f3bd8dcf9d61ac59868db1dd88
KhoaVo0512 <vokhoa0121@gmail.com>
2023-08-02 09:51:11 +0700

    fix play in nft media

--- pages/token/[contractAddress]/[tokenId].tsx
+++ pages/token/[contractAddress]/[tokenId].tsx
@@ -396,7 +396,7 @@ export default function TokenPage() {
                           </p>
                         </div>
 
-                        {/* <div className={styles.eventContainer}>
+                        <div className={styles.eventContainer}>
                           <p className={styles.traitName}>From</p>
                           <p className={styles.traitValue}>
                             {/* {event.log_events[0].decoded.params[0].value?.slice(0, 4)}...
@@ -410,7 +410,7 @@ export default function TokenPage() {
                             {/* {event.log_events[0].decoded.params[1].value?.slice(0, 4)}...
                             {event.log_events[0].decoded.params[1].value?.slice(-2)} */}
                           </p>
-                        </div> */}
+                        </div>
 
                         <div className={styles.eventContainer}>
                           <Link
